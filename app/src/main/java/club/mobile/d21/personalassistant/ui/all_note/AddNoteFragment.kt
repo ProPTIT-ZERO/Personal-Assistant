21c8f46a0b74a546195f053b2979c1d1a6f080f6
ProPTIT-ZERO <hoangquocanh20102003abc@gmail.com>
2023-09-13 09:26:33 +0700

    Chỉnh lại UI cho đẹp tí

--- app/src/main/java/club/mobile/d21/personalassistant/ui/all_note/AddNoteFragment.kt
+++ app/src/main/java/club/mobile/d21/personalassistant/ui/all_note/AddNoteFragment.kt
@@ -1,5 +1,6 @@
 package club.mobile.d21.personalassistant.ui.all_note
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,9 @@ import androidx.fragment.app.Fragment
 import club.mobile.d21.personalassistant.R
 import club.mobile.d21.personalassistant.databinding.FragmentAddNoteBinding
 import java.time.LocalDate
+import java.util.Calendar.*
 
-class AddNoteFragment() : Fragment() {
+class AddNoteFragment : Fragment() {
     private var _binding: FragmentAddNoteBinding? = null
     private val binding get() = _binding!!
     private lateinit var callBackNote: CallBackNote
@@ -17,7 +19,7 @@ class AddNoteFragment() : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentAddNoteBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -26,13 +28,24 @@ class AddNoteFragment() : Fragment() {
         initEvent()
     }
     private fun initEvent() {
+        var selectedDate: LocalDate? = null
+        binding.setDate.setOnClickListener {
+            val calendar = getInstance()
+            val datePickerDialog = DatePickerDialog(requireContext(),{
+                    datePicker, year:Int, month:Int, day:Int ->
+                    selectedDate = LocalDate.of(year, month+1,day)
+                binding.date.text = String.format("%2d / %2d / %4d",day,month,year)
+                binding.date.setBackgroundResource(R.drawable.bg_save_button)
+            },calendar.get(YEAR),
+                calendar.get(MONTH),
+                calendar.get(DAY_OF_MONTH))
+                datePickerDialog.show()
+        }
         binding.addButton.setOnClickListener {
-            if(!handleValidation()){
+            if(selectedDate == null || !handleValidation()){
                 return@setOnClickListener
             }
-            callBackNote.add(LocalDate.of(binding.yearEditText.text.toString().toInt(),
-                binding.monthEditText.text.toString().toInt(),
-                binding.dayEditText.text.toString().toInt()),
+            callBackNote.add(selectedDate!!,
                 binding.noteEditText.text.toString())
             val fragmentManager = parentFragmentManager
             fragmentManager.beginTransaction().remove(this).commit()
@@ -47,18 +60,6 @@ class AddNoteFragment() : Fragment() {
         }
     }
     private fun handleValidation(): Boolean {
-        if(binding.dayEditText.text.toString().isEmpty()){
-            binding.dayEditText.error = getString(R.string.please_enter_the_date)
-            return false
-        }
-        if(binding.monthEditText.text.toString().isEmpty()){
-            binding.monthEditText.error = getString(R.string.please_enter_the_month)
-            return false
-        }
-        if(binding.yearEditText.text.toString().isEmpty()){
-            binding.yearEditText.error = getString(R.string.please_enter_the_year)
-            return false
-        }
         if(binding.noteEditText.text.toString().isEmpty()){
             binding.noteEditText.error = getString(R.string.please_enter_your_note)
             return false
